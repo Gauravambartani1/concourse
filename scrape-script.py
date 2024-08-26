5ddf1eb39332b4ddb7c5251d5092a9cfe821fdd2
Gaurav Ambartani <31471294+Gauravambartani1@users.noreply.github.com>
2024-08-26 16:07:25 +0530

    Update scrape-script.py

--- scrape-script.py
+++ scrape-script.py
@@ -11,7 +11,7 @@ connection_url = URL.create(
     "postgresql",
     username="postgres",
     password="Abcd@1234",
-    host="192.168.3.55",
+    host="172.20.144.1",
     port=5432,
     database="Test_DB",
 )
@@ -46,39 +46,42 @@ if login_response.status_code != 200:
     print("Login failed!")
     print(login_response.content)  # Print the response content for debugging
     exit()
-# URL for Reliance Industries' Profit & Loss data
+
 reliance_url = "https://www.screener.in/company/RELIANCE/consolidated/"
-# Fetch the Reliance Industries page
+
 response = session.get(reliance_url)
 soup = BeautifulSoup(response.content, 'html.parser')
-# Extract Profit & Loss data (assuming it's in a table with id 'profit-loss')
+
 profit_loss_section = soup.find('section', {'id': 'profit-loss'})
 table = profit_loss_section.find('table', {'class': 'data-table'})
 df = pd.read_html(str(table))[0]
-# Reformat the data
+
 df = df.transpose()
-df.columns = df.iloc[0].str.strip().str.replace('+', '').str.replace('%', '')
+df.columns = df.iloc[0].str.strip().str.replace('+', '').str.replace('%', '').str.strip().str.replace(' ', '')
 df = df.iloc[1:]
 df.index.name = 'Financial Metric'
 
 for column in df.columns:
     if df[column].dtype == 'object':
-        df[column] = df[column].str.replace('%', '')  # Remove percentage signs
-        df[column] = pd.to_numeric(df[column], errors='coerce')  # Convert to numeric (float or int)
+        df[column] = df[column].str.replace('%', '')  
+        df[column] = pd.to_numeric(df[column], errors='coerce') 
 
 for col in df.columns:
     if df[col].notna().all() and (df[col] % 1 == 0).all():
         df[col] = df[col].astype('int')
-# Add ID column at the beginning
-df.insert(0, 'ID', range(1, len(df) + 1))
+
+df.insert(0, 'IDs', range(1, len(df) + 1))
 
 # Save the DataFrame to a CSV file
 csv_file = 'reliance_profit_loss.csv'
 df.to_csv(csv_file)
 print(f"Data saved to {csv_file}")
 # Save the DataFrame to a Postgres table
-df.to_sql('reliance_profit_loss', engine, if_exists='replace', index=False, dtype={
-    "ID": Integer(),
+
+
+
+df.to_sql('reliance_profit_loss', engine, if_exists='replace', index=True, dtype={
+    "IDs": Integer(),
     "Sales": Integer(),
     "Expenses": Integer(),
     "Operating Profit": Integer(),
